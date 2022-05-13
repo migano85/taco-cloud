2d45bf778dd1d8f08c9eca25b0279091376386e7
migano85 <mahmoudyacine@yahoo.com>
2022-05-13 21:21:51 +0300

    working with validation

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -3,8 +3,12 @@ package tacos.web;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,11 +65,15 @@ public void addIngredientsToModel(Model model) {
   }
   
   @PostMapping /*("/design")*/ // we can remove @RequestMapping("/design") and add ("/design") here
-  public String processTaco(Taco taco) {
-  // Save the taco...
-  // We'll do this in chapter 3
-  log.info("Processing taco: " + taco);
-  return "redirect:/orders/current";
+  public String processTaco(@Valid @ModelAttribute("taco") Taco taco, Errors errors) {
+	
+	  //********I dont need vlaidation for now it's just waste of learning time****
+//	  if (errors.hasErrors()) {
+//	      return "design";
+//	  }
+	  
+	  log.info("Processing taco: " + taco);
+	  return "redirect:/orders/current";
   }
  
   private Iterable<Ingredient> filterByType(
--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -1,6 +1,9 @@
 package tacos.web;
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +23,12 @@ public class OrderController {
 	}
 	
 	@PostMapping
-	public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+	public String processOrder(@Valid TacoOrder order, Errors errors) {
 	  log.info("Order submitted: {}", order);
-	  sessionStatus.setComplete();
-	 
+//********I dont need vlaidation for now it's just waste of learning time****
+//	  if (errors.hasErrors()) {
+//	      return "orderForm";
+//	  }
 	  return "redirect:/";
 	}
 }
