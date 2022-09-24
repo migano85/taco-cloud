6c389dd393bb13a3d409ac21788a64fda1f919f7
migano85 <mahmoudyacine@yahoo.com>
2022-09-24 11:43:59 +0300

    .

--- taco-cloud/src/main/java/tacos/data/TestRecord.java
+++ taco-cloud/src/main/java/tacos/data/TestRecord.java
@@ -3,12 +3,15 @@ package tacos.data;
 public record TestRecord(Long id, String name) {
 
 	//for more info check : https://www.baeldung.com/java-record-keyword
-	private static String tt = "joe";
+	private static String tt = "tito";
 	
 	public TestRecord {
 		//you can write customize code that get executed when calling the argument constructor, like this
-		System.out.println("inside default constructor  " + name);
-		System.out.println("in default const " + tt);
+		System.out.println("inside argumented constructor  " + name);
+		System.out.println("default const value : " + tt);
+		if(name.equals("joe")) {
+			name = "i am not joe";
+		}
 	}
 	
 	public TestRecord() {
--- taco-cloud/src/main/java/testingThings/classWithMain.java
+++ taco-cloud/src/main/java/testingThings/classWithMain.java
@@ -1,14 +1,33 @@
 package testingThings;
 
+import java.util.Comparator;
+import java.util.Objects;
+
 import tacos.data.TestRecord;
 
 public class classWithMain {
 
 	public static void main(String[] args) {
-		TestRecord tr = new TestRecord();
+
+		TestRecord tr = new TestRecord();//we can add noArgs constructor indirectly
 		System.out.println("id for default" + tr.id());
 		System.out.println(new TestRecord(3L, "number 3").name());
 		
+		
+		Comparator<String> compExample1 = (first, second)->Integer.compare(first.length(), second.length());
+		
+		System.out.println(compExample1.compare("1176", "228"));
+		
+		Comparator<Double> compExample2 = (s, q) -> Objects.equals(q, s)?1:2;//we should return int because this is the return type of compare() the single method in the interface
+		
+		Comparator<Double> compTheOldWay = new Comparator<Double>() {
+			//before java 8 there was inline implementation for interfaces, not only functional interfaces ut even the one which have more than one interface like List
+			
+			@Override
+			public int compare(Double s, Double q) {
+				return Objects.equals(q, s)?1:2;
+			}
+		};
 	}
 
 }
